28b4de7e06f527618ee8219bf6f034cd2dc7c643
minjae <zuppa812@gmail.com>
2022-02-04 14:47:18 +0900

    daum news

---
+++ daum_news.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import urllib.request as req
+
+url = "https://news.daum.net/"
+result = req.urlopen(url)
+soup = BeautifulSoup(result, "html.parser")
+
+news = soup.select("strong.tit_g")
+
+for list in news :
+    a = list.select_one("a")
+    print("링크 : " + a.get('href'))
+
+    title = a.sting
+    print("제목 : " + title)
